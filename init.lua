7b6b90c8cd2091388ee8a0c99e1a0d484de0aa3b
Abees Khan <abeeskhan@DBITLAP0168.local>
2024-06-14 15:58:20 +0530

    Updated scrolloffline from 8 to 12

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 12
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
